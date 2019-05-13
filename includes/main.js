fb9395fec1c1ff1fd36ea00594ccfb5715438508
amac2d <amac2@student.mtsac.edu>
2019-05-13 09:12:04 -0700

    root js final gallery 051119

--- includes/main.js
+++ includes/main.js
@@ -25,6 +25,7 @@ function initiateApp(){
 	*/
 	makeGallery(pictures);
 	addModalCloseHandler();
+
 }
 function makeGallery(imageArray){
 	/*
@@ -53,8 +54,14 @@ function makeGallery(imageArray){
 	 */
 
 	//use loops and jquery dom creation to make the html structure inside the #gallery section
-	for(var figNum = 0; figNum < 6 ; figNum++){
-		var figure + figNum =
+
+	for(var figNum = 0; figNum < pictures.length ; figNum++){
+		var figure = $('<figure>', {'class': 'imageGallery col-xs-12 col-sm-6 col-md-4', 'style': 'background-image:url(' + pictures[figNum] + ');', 'onclick': 'displayImage()'});
+		var text1 = '' + pictures[figNum];
+		var textSlice = text1.slice(7, );
+		var figCaption = $('<figcaption>').append(textSlice);
+		$(figure).append(figCaption);
+		$('#gallery').append(figure);
 	}
 
 	//create a loop to go through the pictures
@@ -65,15 +72,39 @@ function makeGallery(imageArray){
 		//append the element to the #gallery section
 
 }
+/*
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#galleryModal">Open Modal</button>
+
+  <div class="modal fade" id="galleryModal" role="dialog">
+    <div class="modal-dialog">
+
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">landscape-10</h4>
+        </div>
+        <div class="modal-body">
+          <img src="images/landscape-10.jpg">
+        </div>
+      </div>
+
+    </div>
+  </div>
+ */
+
+
 
 function addModalCloseHandler(){
 	//add a click handler to the img element in the image modal.  When the element is clicked, close the modal
-	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp	
+	//for more info, check here: https://www.w3schools.com/bootstrap/bootstrap_ref_js_modal.asp
+
 }
 
 function displayImage(){
 	//find the url of the image by grabbing the background-image source, store it in a variable
 	//grab the direct url of the image by getting rid of the other pieces you don't need
+
 
 	//grab the name from the file url, ie the part without the path.  so "images/pexels-photo-132037.jpeg" would become
 		// pexels-photo-132037
